bbf9e37683c4f746aa9ab9c9a0129d5feeff6e49
EC2 Default User <ec2-user@ip-172-31-20-39.ec2.internal>
2022-03-22 09:20:26 +0000

    made page[public(addresses#index, addresses#edit)]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_19_080426) do
+ActiveRecord::Schema.define(version: 2022_03_21_115227) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
