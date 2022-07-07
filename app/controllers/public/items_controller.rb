c5440c4da0747b1099055cb7685d626028e930dc
EC2 Default User <ec2-user@ip-172-31-20-39.ec2.internal>
2022-07-07 13:32:00 +0000

    change the title of page items#index

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,6 +3,7 @@ class Public::ItemsController < ApplicationController
   def index
     @items = params[:genre_id].present? ? Genre.find(params[:genre_id]).items : Item.all
     #viewで指定したパラメータgenre_idが存在すれば，genre_idで絞り込み．存在しなければ全商品取得．
+    @genre = params[:genre_id].present? ? Genre.find(params[:genre_id]).name : "商品"
     @genres = Genre.all
   end
 
